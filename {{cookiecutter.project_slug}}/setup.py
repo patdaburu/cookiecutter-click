72d4ff6fd933e09d95fc52697552ae33a4878015
patdaburu <pat@daburu.net>
2018-07-07 05:04:13 -0500

    Fixed environment.yml and setup.py.

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -73,6 +73,6 @@ setup(
 
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
-    'Programming Language :: Python :: {{cookiecutter.license}}',
+    'Programming Language :: Python :: {{cookiecutter.python_version}}',
   ],
 )
