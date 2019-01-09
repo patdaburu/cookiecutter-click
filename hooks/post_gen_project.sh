550ce46a69020641f476d380ccefd23a4b9db8d1
Pat Daburu <patdaburu@gmail.com>
2019-01-09 08:25:39 -0600

    Updated click version.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -28,6 +28,6 @@ cat << "LOGO"
 LOGO
 
 echo -e "\033[1m\033[4mNext Steps\033[0m"
-echo -e "\033[32mcd {{cookiecutter.package_name}}\033[0m"
+echo -e "\033[32mcd {{cookiecutter.project_name}}\033[0m"
 echo -e "\033[32msource venv/bin/activate\033[0m"
 echo -e "\033[32m{{cookiecutter.cli_name}} --help\033[0m"
