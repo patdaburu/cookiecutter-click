cad48057ebb6062ba80cd4667ee7e14ccf14abfc
patdaburu <pat@daburu.net>
2019-09-26 06:11:42 -0500

    Added options for alabaster or readthedocs.

--- {{cookiecutter.project_name}}/docs/source/conf.py
+++ {{cookiecutter.project_name}}/docs/source/conf.py
@@ -59,14 +59,23 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+
+{% if cookiecutter.sphinx_theme == 'alabaster' %}
+html_theme='alabaster'
 
 html_theme_options = {
     'logo': 'logo.png',
     'github_user': '{{cookiecutter.github_user}}',
     'github_repo': '{{cookiecutter.project_name}}',
 }
+{% endif %}
+{% if cookiecutter.sphinx_theme == 'readthedocs' %}
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+
+}
+{% endif %}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
