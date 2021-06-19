f6042373f231e5bbc57bcecb621c29c58af2ee43
vlad doster <mvdoster@gmail.com>
2021-06-19 05:07:30 -0500

    (feat) pipenv
    - clean up README.md
    - clean up Makefile
    - add `pipenv` option
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,26 +1,26 @@
-#!/usr/bin/env python
-import os
+#!/usr/bin/env python3
+import os, subprocess
 
 TERMINATOR = "\x1b[0m"
-WARNING = "\x1b[1;33m [WARNING]: "
-INFO = "\x1b[1;33m [INFO]: "
-HINT = "\x1b[3;33m"
 SUCCESS = "\x1b[1;32m [SUCCESS]: "
 
-def remove_pylintrc_file():
-    os.remove(".pylintrc")
-    
-def remove_flake8_file():
-    os.remove(".flake8")
+def remove_file(f_name):
+    os.remove(f_name)
 
 def main():
     linter = "{{ cookiecutter.linter }}".lower()
-    if linter == "flake8":
-        remove_pylintrc_file()
-    if linter == "pylint":
-        remove_flake8_file()
+    if linter != "flake8":
+        remove_file(".flake8")
+    if linter != "pylint":
+        remove_file(".pylintrc")
 
-    print(SUCCESS + "Project initialized, keep up the good work!" + TERMINATOR)
+    venv = "{{ cookiecutter.virtualenv }}".lower()
+    if venv == "pipenv":
+        subprocess.run(["pip3", "install", "pipenv"])
+    if venv == "virtualenv":
+        subprocess.run(["pip3", "install", "virtualenv"])
+
+    print(f"{SUCCESS} Project initialized, keep up the good work!{TERMINATOR}")
 
 
 if __name__ == "__main__":
